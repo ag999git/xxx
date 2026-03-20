821691726512dc93664bb6df5af22be4df095f28
Anurag Gupta <62739090+ag999git@users.noreply.github.com>
2026-03-20 22:03:01 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,22 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+project = 'My Python Book'
+author = 'Your Name'
+release = '1.0'
+
+extensions = [
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
